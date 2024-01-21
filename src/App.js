47f6b0f7466d89920a5a41807fc4509a4e51752a
pk1260 <pascalkuiper@live.nl>
2024-01-21 20:55:00 +0100

    feat: Set up basic weather display and integrate OpenWeatherMap API"
    Implemented the initial structure for displaying weather information in the App component. Integrated the OpenWeatherMap API with a hardcoded URL for Amsterdam. The component includes placeholder elements for the weather icon, description, temperature, humidity, and wind speed.
    
    Details:
    - Set up basic layout for weather display.
    - Integrated OpenWeatherMap API with a hardcoded URL for Amsterdam.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const WeatherAPI = 'https://api.openweathermap.org/data/2.5/weather?q=amsterdam&appid=' + process.env.REACT_APP_WEATHER_KEY;
+
+    console.log(WeatherAPI)
+    return (
+        <div className="App">
+            <section className="app_container">
+                <h2 className="weather_city">Amsterdam</h2>
+                <img src="#" alt="weather_icon"/>
+                <p className="weather_description">25°C</p>
+                <section className="weather_info">
+                    <p className="weather_temperature">19°C</p>
+                    <p className="weather_humidity">20%</p>
+                    <p className="weather_wind">10KM/H</p>
+                </section>
+            </section>
+        </div>
+    );
 }
 
 export default App;
