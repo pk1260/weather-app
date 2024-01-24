46b589d0b0cfd019e3ac5fe4d804c4c307e22da2
pk1260 <pascalkuiper@live.nl>
2024-01-25 00:02:24 +0100

    feat: Enhance error handling and refine layout/styling
    - Implemented error handling for cases where the searched city is not found.
    - Updated the layout for a more refined and user-friendly design.
    - Adjusted styling to improve the overall visual appearance of the app.

--- src/App.js
+++ src/App.js
@@ -4,43 +4,51 @@ import axios from 'axios';
 function App() {
     const [data, setData] = React.useState();
     const [location, setLocation] = useState('');
-    const WeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=` + process.env.REACT_APP_WEATHER_KEY + `&units=metric`
+    const WeatherAPI = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=` + process.env.REACT_APP_WEATHER_KEY + `&units=metric`;
 
     const searchLocation = (event) => {
         if (event.key === "Enter") {
-            axios.get(WeatherAPI).then(res => {
-                const data = res.data;
-                setData(data);
-                console.log(data)
-            })
+            axios.get(WeatherAPI)
+                .then(res => {
+                    const data = res.data;
+                    setData(data);
+                    console.log(data)
+            }).catch(error => {
+                console.error("Error fetching weather data:", error);
+                // Handle the error, e.g., show a user-friendly message
+                alert("Could not fetch weather data. Please check the city name and try again.");
+            });
         }
     }
 
     return (
         <div className="App">
-            <section className="app_container">
-                <section className="weather_input">
-                    <input
-                        value={location}
-                        onChange={(event) => setLocation(event.target.value)}
-                        onKeyDown={searchLocation}
-                        placeholder="Enter Location"
-                        type="text"
-                    />
-                </section>
-                {data && (
-                    <>
-                        <h2 className="weather_city">{data.name}</h2>
-                        <p className="weather_description">{data.main.temp.toFixed()}°C</p>
-                        <img src={`assets/${data.weather[0].icon}.png`} alt="weather_icon"/>
-                        <section className="weather_info">
-                            <p className="weather_temperature">{data.main.feels_like.toFixed()}°C</p>
-                            <p className="weather_humidity">{data.main.humidity}%</p>
-                            <p className="weather_wind">{data.wind.speed.toFixed()}KM/H</p>
-                        </section>
-                    </>
-                )}
+            <section className="weather_input">
+                <input
+                    value={location}
+                    onChange={(event) => setLocation(event.target.value)}
+                    onKeyDown={searchLocation}
+                    placeholder="Enter Location"
+                    type="text"
+                />
             </section>
+            {data && (
+                <>
+                    <section className="content_container">
+                        <section className="weather_card_header">
+                            <h1>Daily Forecast</h1>
+                        </section>
+                        <h2 className="weather_city">{data.name}</h2>
+                        <figure className="weather_figure">
+                            <img src={`assets/${data.weather[0].icon}.png`} alt="weather_icon"/>
+                        </figure>
+                        <section className="weather_info">
+                            <p className="weather_temp">{data.main.temp.toFixed()}°C</p>
+                            <p className="weather_desc">{data.weather[0].description}</p>
+                        </section>
+                    </section>
+                </>
+            )}
         </div>
     );
 }
